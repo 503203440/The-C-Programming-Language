7aa77d399663d8dc461149441b50b1b844180d32
YX <503203440@qq.com>
2019-03-13 15:53:19 +0800

    将文件重命名为cleanUpTheDirectory

--- cleanUpTheDirectory.bat
+++ cleanUpTheDirectory.bat
@@ -1,7 +1,7 @@
-@REM @echo off表示关闭
+@REM @echo off表示关闭命令语句的输出
 @echo off
 
-REM chcp 65001表示将控制台代码设置为utf-8格式
+REM chcp 65001表示将控制台代码设置为utf-8格式，chcp 936表示将控制台代码设置为GBK格式
 chcp 936
 
 REM color 3f表示将控制台颜色设置为蓝色
@@ -14,6 +14,8 @@ REM 判断flag的值,注意if条件中如果内容比较多需要换行的话，后面的括号"("必须留在和
 if %flag% == y (
 
     if exist *.exe (
+
+        REM 这里指匹配所有文件类型为exe的文件，后面的参数/f表示可删除只读文件，/s表示可删除系统文件，/q表示取消删除前的系统确认提示
         del *.exe /f /s /q 
         echo 已删除所有生成的.exe文件
     ) else (
